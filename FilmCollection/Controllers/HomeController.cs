3d415be8b8333fa3c9c417e77b19f2edf00b4776
Trevor Heath <trevormheath@gmail.com>
2023-02-13 01:17:56 -0700

    Add project files.

---
+++ FilmCollection/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace FilmCollection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private FilmDataContext _filmContext { get; set; }
+
+        public HomeController(ILogger<HomeController> logger, FilmDataContext filmContext)
+        {
+            _logger = logger;
+            _filmContext = filmContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Podcasts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieForm(FilmData film)
+        {
+            _filmContext.Add(film);
+            _filmContext.SaveChanges();
+
+            return View("Confirmation", film);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FilmCollection/Models/FilmDataContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmCollection.Models
+{
+    public class FilmDataContext : DbContext
+    {
+        public FilmDataContext (DbContextOptions<FilmDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<FilmData> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<FilmData>().HasData(
+                new FilmData
+                {
+                    FilmId = 1,
+                    Category = "Musical",
+                    Title = "The Greatest Showman",
+                    Year = 2017,
+                    Director = "Michael Gracey",
+                    Rating = "PG",
+                    Edited = "no",
+                    Notes = "Mom's favorite movie"
+                },
+                new FilmData
+                {
+                    FilmId = 2,
+                    Category = "Fantasy",
+                    Title = "Harry Potter: The Deathly Hallows Pt. 2",
+                    Year = 2011,
+                    Director = "David Yates",
+                    Rating = "PG-13",
+                    Edited = "yes",
+                    Lent_To = "Older Brother"
+                },
+                new FilmData
+                {
+                    FilmId = 3,
+                    Category = "Family",
+                    Title = "Kung Fu Panda",
+                    Year = 2008,
+                    Director = "John Stevenson, Mark Osborne",
+                    Rating = "PG",
+                    Edited = "no"
+                }
+            );
+        }
+    }
+}
